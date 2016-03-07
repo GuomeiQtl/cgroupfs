47304591d8e3a6fdd453fbbf7dcb388ed805a0da
root <root@localhost.localdomain>
2016-03-07 12:57:40 +0800

    init

---
+++ cgroupfs.go
@@ -0,0 +1,54 @@
+package cgroupfs
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"bazil.org/fuse"
+	fusefs "bazil.org/fuse/fs"
+	_ "bazil.org/fuse/fs/fstestutil"
+
+	"github.com/chenchun/cgroupfs/fs"
+)
+
+func Serve(mountPoint, cgroupDir, vethName string) error {
+	c, err := fuse.Mount(
+		mountPoint,
+		fuse.FSName("cgroupfs"),
+		fuse.Subtype("cgroupfs"),
+		fuse.LocalVolume(),
+		fuse.VolumeName("cgroup volume"),
+	)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	go handleStopSignals(mountPoint)
+
+	err = fusefs.Serve(c, fs.FS{cgroupDir, vethName})
+	if err != nil {
+		return err
+	}
+
+	// check if the mount process has an error to report
+	<-c.Ready
+	if err := c.MountError; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func handleStopSignals(mountPoint string) {
+	s := make(chan os.Signal, 10)
+	signal.Notify(s, os.Interrupt, syscall.SIGTERM, syscall.SIGSTOP)
+
+	for range s {
+		if err := fuse.Unmount(mountPoint); err != nil {
+			log.Fatal("Error umounting %s: %s", mountPoint, err)
+		}
+		os.Exit(0)
+	}
+}
---
+++ fs/dir.go
@@ -0,0 +1,82 @@
+package fs
+
+import (
+	"os"
+	"path/filepath"
+	"sync"
+
+	"bazil.org/fuse"
+	fusefs "bazil.org/fuse/fs"
+	_ "bazil.org/fuse/fs/fstestutil"
+
+	"github.com/opencontainers/runc/libcontainer/cgroups"
+
+	"golang.org/x/net/context"
+)
+
+const (
+	_ = iota
+	INODE_DIR
+	INODE_HELLO
+	INODE_MEMINFO
+	INODE_DISKSTATS
+	INODE_CPUINFO
+	INODE_STAT
+	INODE_NET_DEV
+)
+
+var (
+	fileMap = make(map[string]FileInfo)
+
+	direntsOnce sync.Once
+	dirents     []fuse.Dirent
+)
+
+// Dir implements both Node and Handle for the root directory.
+type Dir struct {
+	cgroupdir string
+	vethName  string
+}
+
+type FileInfo struct {
+	initFunc   func(cgroupdir string) fusefs.Node
+	inode      uint64
+	subsysName string
+}
+
+func (Dir) Attr(ctx context.Context, a *fuse.Attr) error {
+	a.Inode = INODE_DIR
+	a.Mode = os.ModeDir | 0555
+	return nil
+}
+
+func (d Dir) Lookup(ctx context.Context, name string) (fusefs.Node, error) {
+	if name == "hello" {
+		return File{}, nil
+	} else if fileInfo, ok := fileMap[name]; ok {
+		var path string
+
+		if len(fileInfo.subsysName) == 0 {
+			path = d.vethName
+		} else {
+			mountPoint, err := cgroups.FindCgroupMountpoint(fileInfo.subsysName)
+			if err != nil {
+				return nil, fuse.ENODATA
+			}
+			path = filepath.Join(mountPoint, d.cgroupdir)
+		}
+
+		return fileInfo.initFunc(path), nil
+	}
+	return nil, fuse.ENOENT
+}
+
+func (Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+	direntsOnce.Do(func() {
+		dirents = append(dirents, fuse.Dirent{Inode: INODE_HELLO, Name: "hello", Type: fuse.DT_File})
+		for k, v := range fileMap {
+			dirents = append(dirents, fuse.Dirent{Inode: v.inode, Name: k, Type: fuse.DT_File})
+		}
+	})
+	return dirents, nil
+}
